6c20fb9b3a5f10e4cb7db08f2ea0a1f1b407d3a1
lucasluang <lucasluang@yahoo.com.br>
2020-03-19 17:21:36 -0300

    Create SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import com.mysql.fabric.xmlrpc.base.Data;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Lucas", "lucasluang@gmail.com", 
 				new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
